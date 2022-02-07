227275c4875fec7571d69ebf5cc3ddbf6bca3bc6
Aidan Loten <asloten@gmail.com>
2022-02-07 12:19:42 -0500

    first commit after initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'likeMindsApp';
+export class AppComponent implements OnInit {
+  public users: User[];
+
+  constructor(private userService: UserService) {
+    this.users = [];
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  public getUsers(): void {
+    this.userService.getUsers().subscribe(
+      (response: User[]) => {
+        this.users = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
